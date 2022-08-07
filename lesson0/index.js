961b1c1a9431a02b9f3c9f67b1759d94a6e6eda8
sae-github <miyasae1126@gmail.com>
2022-08-08 07:48:46 +0900

    feat: lesson0

---
+++ lesson0/index.js
@@ -0,0 +1,4 @@
+// 関数greeting は "hello!world!"など、任意の文字列返す関数です。 引数valueに型注釈してください
+// const greeting = (value) => "hello!" + value;
+var greeting = function (value) { return "hello!" + value; };
+console.log(greeting("Sae"));
---
+++ lesson0/index.ts
@@ -0,0 +1,6 @@
+// 関数greeting は "hello!world!"など、任意の文字列返す関数です。 引数valueに型注釈してください
+// const greeting = (value) => "hello!" + value;
+
+const greeting = (value: string) => "hello!" + value;
+
+console.log(greeting("Sae"));
